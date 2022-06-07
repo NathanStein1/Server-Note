442b2fae8a4208ffdf4b113f55d74e0861c00d5f
NathanStein1 <nathanstein2@gmail.com>
2022-06-07 11:33:45 -0700

    Meaningful comment

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const PORT = 3001;
+const app = express();
+const {v4:uuidv4} = require('uuid');
+// Sets up the Express app to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
+
+});
+
+app.get('/notes', (req, res) => {
+  console.log(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    let newNote = req.body;
+    newNote.id= uuidv4()
+    let notes = JSON.parse(data)
+    notes.push(newNote);
+  
+  fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    res.json(newNote)
+  })
+  })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    
+    let notes = JSON.parse(data);
+    let filterNotes = notes.filter(note => note.id !== req.params.id)
+    
+  
+  fs.writeFile('./db/db.json', JSON.stringify(filterNotes), (err) => {
+    if (err) throw err;
+    res.json(filterNotes)
+  })
+  })
+})
+
+// Enter the note,
+// throw the note into db.json
+app.listen(PORT, () => {
+  console.log(`App at http://localhost:${PORT}`);
+});
+
+
+
+// fetch - done post data
+
+// make a routes that does the foolowing
+
+// 1. add id to the note
+// 2. read the note
+// 3. update the read notes
+// 4. write the notes back to db.json
