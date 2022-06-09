42f2d1aa1f8136b94c9e301d6ca9ed2dfa4a795d
NathanStein1 <nathanstein2@gmail.com>
2022-06-08 17:47:31 -0700

    new stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const {v4:uuidv4} = require('uuid');
 // Sets up the Express app to handle data parsing
